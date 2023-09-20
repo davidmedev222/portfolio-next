993bb12eb219249edc2c23486386a6e966ac91e0
davidmedev222 <davidmedev@gmail.com>
2023-09-20 20:24:53 -0300

    feat(ui): add UI highlights experience component of the about page

--- src/components/heading/PageHeading.tsx
+++ src/components/heading/PageHeading.tsx
@@ -1,13 +1,33 @@
+import clsx from 'clsx'
 import Divider from '../divider/Divider'
 
 interface Props {
   children: React.ReactNode
+  variant?: 'sub-heading'
+  as?: 'h2' | 'h3' | 'h4'
+  className?: string
 }
 
-function PageHeading({ children }: Props) {
+function PageHeading({ children, variant, as, className }: Props) {
+  const classes = {
+    section: clsx('pt-[84px] md:space-y-4 md:pt-[152px]', variant === 'sub-heading' && 'pt-11 md:pt-20', className),
+    heading: clsx('text-center font-benzin text-[min(6vw,36px)] uppercase')
+  }
+
+  if (as) {
+    return (
+      <section className={classes.section}>
+        {as === 'h2' && <h2 className={classes.heading}>{children}</h2>}
+        {as === 'h3' && <h3 className={classes.heading}>{children}</h3>}
+        {as === 'h4' && <h4 className={classes.heading}>{children}</h4>}
+        <Divider direction='horizontal' />
+      </section>
+    )
+  }
+
   return (
-    <section className='pt-[84px] md:space-y-4 md:pt-[152px]'>
-      <h1 className='text-center font-benzin text-[min(6vw,36px)] uppercase'>{children}</h1>
+    <section className={classes.section}>
+      <h1 className={classes.heading}>{children}</h1>
       <Divider direction='horizontal' />
     </section>
   )
