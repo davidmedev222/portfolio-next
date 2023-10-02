73eaf638ba7f9e0280adae1344f9467ea812839c
davidmedev222 <davidmedev@gmail.com>
2023-10-02 18:38:27 -0300

    chore(metatags): add files at the matcher in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,5 +31,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|apple-icon.png|icon.png|opengraph-image.png|opengraph-image.alt.txt).*)'
+  ]
 }
